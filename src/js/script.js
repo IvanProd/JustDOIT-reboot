6606b54a75e1aa01d3dbebe1d60679f61254160c
Ivan Prodeus <learning.webpractice@gmail.com>
2021-05-19 18:37:57 +0300

    обновление
    объединил скрипты
    внес правку в index.html
    внес правки в registration.html и scss

--- src/js/script.js
+++ src/js/script.js
@@ -22,5 +22,17 @@
       menuBtnRef.setAttribute("aria-expanded", !expanded)
       mobileMenuRef.classList.toggle("is-open")
     })
-  })()
-    
+  })();
+
+  (() => {
+    const menuItmRef = document.querySelectorAll('[data-menu-item]');
+    const mobileMenuRef = document.querySelector('[data-menu]');
+    const menuBtnRef = document.querySelector('[data-menu-button]');
+  
+    function closeMenu() {
+      mobileMenuRef.classList.remove('is-open');
+      menuBtnRef.classList.toggle('is-open');
+    }
+    Array.from(menuItmRef).forEach(item => item.addEventListener('click', closeMenu));
+  })();
+  
